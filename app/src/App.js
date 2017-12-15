858ffbd4a98bb73f16e662531a8cebde83de2f12
Loukas Anastasiou <loukas_anastasiou@outlook.com>
2017-12-15 06:23:08 +0100

    Initial commit

---
+++ app/src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
+
+import SearchForm from './components/SearchForm';
+import Navigation from './components/Navigation';
+import NotFound from './components/NotFound';
+import Page from './components/Page';
+import './App.css';
+import apiKey from './config';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: [],
+      currentPage: ""
+    }
+  }
+
+  getPhotosForPage(currentPage, title) {
+    if((currentPage !== title) && title !== "Search") {
+      if(title === "Home") {
+        this.getRecentPhotos();
+      } else {
+        this.performSearch(title);
+      }
+    }
+  }
+
+  getRecentPhotos() {
+    axios.get('https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=' + apiKey + '&format=json&nojsoncallback=1')
+    .then(result => {
+      this.setState({photos: result.data.photos.photo});
+      console.log(this.state.photos);
+    })
+    .catch(error => {
+      console.log("Error occured while fetching data from Flickr: " + error.message);
+    });
+  }
+
+  performSearch(term) {
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${term}&format=json&nojsoncallback=1`)
+      .then(result => {
+        this.setState({photos: result.data.photos.photo});
+        console.log(this.state.photos);
+      })
+      .catch(error => {
+        console.log("Error occured while fetching data from Flickr: " + error.message);
+      });
+  }
+
+  changePageName(currentPage) {
+      this.setState({currentPage});
+  }
+
+  render() {
+    return (
+      <BrowserRouter basename="/react-flickr-gallery">
+        <Switch>
+          <Route exact path='/' render={ () => <Page title="Home" {...this.state}  getPhotos={(page,title) => this.getPhotosForPage(page,title)} changePageName={title => this.changePageName(title)} fetchPhotos={term => this.performSearch(term)} />} />
+          <Route path='/cats' render={() => <Page title="Cats" {...this.state}  getPhotos={(page,title) => this.getPhotosForPage(page,title)} changePageName={title => this.changePageName(title)} fetchPhotos={term => this.performSearch(term)} />} />
+          <Route path='/dogs' render={() => <Page title="Dogs" {...this.state}  getPhotos={(page,title) => this.getPhotosForPage(page,title)} changePageName={title => this.changePageName(title)} fetchPhotos={term => this.performSearch(term)} />} />
+          <Route path='/computers' render={() => <Page title="Computers" {...this.state}  getPhotos={(page,title) => this.getPhotosForPage(page,title)} changePageName={title => this.changePageName(title)} fetchPhotos={term => this.performSearch(term)} />} />
+          <Route path='/search' render={() => <Page title="Search" {...this.state}  getPhotos={(page,title) => this.getPhotosForPage(page,title)} changePageName={title => this.changePageName(title)} fetchPhotos={term => this.performSearch(term)} />} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ app/src/config.js
@@ -0,0 +1,2 @@
+const apiKey = '9cb419f4e108177efce848b305ef1c8d';
+export default apiKey;
