8777b77931fe663022f69a86537bcce124c1fd12
Loukas Anastasiou <loukas_anastasiou@outlook.com>
2017-12-27 06:31:38 +0100

    Added btn
    Added btn

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -5,6 +5,7 @@ import NavItem from './NavItem';
 const Navigation = (props) => (
   <nav className="main-nav">
     <ul>
+      <NavItem url="/search" name="search"/>
       <NavItem url="/cats" name="cats"/>
       <NavItem url="/dogs" name="dogs"/>
       <NavItem url="/computers" name="computers" />
